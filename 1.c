1b20cc935c893498c4b0b1b8a040f728f7091b11
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-04 20:56:01 +0530

    created 1.c

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // maximum marks in each subject is taken to be 100
+    int m1, m2, m3, m4, m5, sum;
+    scanf("%d %d %d %d %d", &m1, &m2, &m3, &m4, &m5);
+    sum = m1+m2+m3+m4+m5;
+    float page = (float) sum / 5;
+    
+    printf("Total marks obtained : %d\n", sum);
+    printf("Percentage obtained : %.2f%c\n", page, '%');
+
+    return 0;
+}
