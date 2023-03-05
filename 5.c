9649ba7ba7863e47b0ddd7032d33723c38b8b344
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-05 17:22:47 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,21 @@
+// Task : WAP that swaps values of two variables using a third variable
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+    
+    // inputting 2 numbers
+    scanf("%d%d", &a, &b);
+    
+    // print the values before swaping
+    printf("a : %d    b : %d\n", a, b);
+    
+    // swaping the values
+    temp = a;
+    a = b;
+    b = temp;
+    
+    // printing the values after swaping
+    printf("a : %d    b : %d\n", a, b);
+    
+    return 0;
