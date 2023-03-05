7b2a9473d0989ad8559d34b915a96381c01bf057
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-05 17:13:57 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,22 @@
+// WAP to calculate the area and circumference of a circle
+#include <stdio.h>
+
+// defining the constant
+#define pi 3.14
+
+int main() {
+    int r;
+
+    // taking r (radius) as input
+    printf("radius? ");
+    scanf("%d", &r);
+    
+    // calculating area and circumference
+    float area = pi*r*r;
+    float cf = 2*pi*r;
+    
+    // printing area and circumference
+    printf("area : %.2f\ncircumeference : %.2f", area, cf);
+    
+    return 0;
+}
