8c8ffa29c73490617d22874e25f725548a130d54
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-05 21:01:49 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,17 @@
+// Task : WAP that finds whether a given number is even or odd
+#include <stdio.h>
+
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+    
+    if(n%2==0){
+        printf("the number is even\n");
+    }
+    else{
+        printf("the number is odd\n");
+    }
+    
+    return 0;
+}
