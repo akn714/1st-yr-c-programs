ac63e181fc83bac69357415e59e0e023c873f586
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-06 14:04:00 +0530

    Create 16.c

---
+++ 16.c
@@ -0,0 +1,30 @@
+// Task : WAP to check whether the entered number is prime or not
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int n, i;
+    
+    // inputting a number
+    scanf("%d", &n);
+    
+    // checking for prime number
+    bool isPrime = true;
+    for(i=2;i<n;i++){
+        if(n%i==0){
+            isPrime = false;
+            break;
+        }
+    }
+    
+    // printing if the number is prime or not
+    if(isPrime){
+        printf("%d is a prime number\n", n);
+    }
+    else{
+        printf("%d is not a prime number\n", n);
+    }
+    
+    return 0;
+}
