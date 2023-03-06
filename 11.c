711143363b78031d6f09543b1e60f1f0d21002eb
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-06 13:34:48 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,31 @@
+// Task : WAP that takes two operands and one operator from the user, perform the operation, and prints the result by using Switch statement
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    char optr;
+    scanf("%d %c %d", &n, &optr, &m);
+    
+    switch(optr){
+        case '+':
+            printf("%d %c %d = %d\n", n, optr, m, n+m);
+            break;
+        case '-':
+            printf("%d %c %d = %d\n", n, optr, m, n-m);
+            break;
+        case '*':
+            printf("%d %c %d = %d\n", n, optr, m, n*m);
+            break;
+        case '/':
+            printf("%d %c %d = %.2f\n", n, optr, m, (float) n/m);
+            break;
+        case '%':
+            printf("%d %c %d = %d\n", n, optr, m, n%m);
+            break;
+        default:
+            printf(" invalid operator\n");
+            break;
+    }
+
+    return 0;
+}
