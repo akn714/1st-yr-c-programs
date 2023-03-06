2a68b4161c08327a48330d3434226c918ca24615
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-06 13:48:53 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,27 @@
+// Task : WAP  to find the factorial of a given number
+
+#include <stdio.h>
+
+int main() {
+    int n, fact = 1, i;
+    
+    // inputting a number
+    scanf("%d", &n);
+    
+
+    // finding factorial of n using increment operator
+    for(i=1;i<=n;i++){
+        fact *= i;
+    }
+    printf("factorial of %d using increment operator: %d\n", n, fact);
+
+    
+    // finding factorial of n using decrement operator
+    fact = 1;
+    for(i=n;i>=1;i--){
+        fact *= i;
+    }
+    printf("factorial of %d using decrement operator: %d\n", n, fact);
+    
+    return 0;
+}
