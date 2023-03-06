d7e30fc36a77ffa3beb8ee0da5d1085b9caa06b9
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-06 19:01:51 +0530

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,40 @@
+// Task : WAP to find the minimum and maximum element of the array
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int n, i;
+    
+    // inputtting the size of array
+    printf("size of array? ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    // inputting the array elements
+    printf("enter elements of the array : ");
+    for(i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("which element do you want to find : ");
+    int toFind;
+    scanf("%d", &toFind);
+    
+    bool isFound = false;
+    for(i=0;i<n;i++){
+        if(toFind==arr[i]){
+            isFound = true;
+            break;
+        }
+    }
+    
+    if(isFound){
+        printf("%d found on index %d\n", toFind, i);
+    }
+    else{
+        printf("%d doesn't exist in the given array!\n", toFind);
+    }
+
+    return 0;
+}
